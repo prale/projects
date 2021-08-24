6be6c8e0f4a0d0222a7f719a1b7054089dfbc9d1
prale <raleprathamesh736@gmail.com>
2021-08-24 15:17:26 -0400

    new1 commit

---
+++ s3_pro/main.tf
@@ -0,0 +1,10 @@
+# Use AWS Terraform provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create s3 bucket
+resource "aws_s3_bucket" "tf_s3" {
+  bucket = "terraform-bucket-vaibhav"
+  acl = "private"
+}

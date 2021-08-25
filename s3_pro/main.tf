5444c69194a59d4d5dd4d87019b0765ea5a29020
prale <raleprathamesh736@gmail.com>
2021-08-25 17:32:59 -0400

    with buck policy

--- s3_pro/main.tf
+++ s3_pro/main.tf
@@ -6,5 +6,33 @@ provider "aws" {
 # Create s3 bucket
 resource "aws_s3_bucket" "tf_aws_s3" {
   bucket = var.bucket_name
-  acl = "private"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_policy" "tf_aws_s3" {
+  bucket = aws_s3_bucket.tf_aws_s3.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression's result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "MYBUCKETPOLICY"
+    Statement = [
+      {
+        Sid       = "IPAllow"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.b.arn,
+          "${aws_s3_bucket.b.arn}/*",
+        ]
+        Condition = {
+          IpAddress = {
+            "aws:SourceIp" = "8.8.8.8/32"
+          }
+        }
+      },
+    ]
+  })
 }

f1242536fb7c09a6e720e1a9cae718f22f357c27
prale <raleprathamesh736@gmail.com>
2021-09-10 15:04:28 -0400

    final4

--- final_pro/backend.tf
+++ final_pro/backend.tf
@@ -1,7 +1,7 @@
-/*terraform {
+terraform {
   backend "s3" {
-    bucket = "wbplay-vdm"
-    key    = "log/terrraform.tfstate"
+    bucket = "wba-mybucket"
+    key    = "logs/terrraform.tfstate"
     region = "us-east-1"
   }
-}*/
+}

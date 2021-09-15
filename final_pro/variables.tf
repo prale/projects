c8f7401d99f151c0740e340e5ec0ecdad3a71e40
prale <raleprathamesh736@gmail.com>
2021-09-15 16:39:33 -0400

    commit34

--- final_pro/variables.tf
+++ final_pro/variables.tf
@@ -39,17 +39,17 @@ variable "vendor_vpc_id" {
 variable "read" {
   type        = bool
   description = "Allow reading of the bucket"
-  default     = null
+  //default     = null
 }
 variable "write" {
   type        = bool
   description = "Allow writing in the bucket"
-  default     = null
+  //default     = null
 }
 variable "read_write" {
   type        = bool
   description = "Allow read and write in the bucket"
-  default     = null
+  //default     = null
 }
 
 //key configuration

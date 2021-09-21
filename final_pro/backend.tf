efd65ee45f7eea32d2ddd89052ba019a69758e6b
prale <raleprathamesh736@gmail.com>
2021-09-21 16:50:01 -0400

    commitere

--- final_pro/backend.tf
+++ final_pro/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "wba-mybucket"
-    key    = "logs/${var.vendor_name}/terrraform.tfstate"
-    region = "us-east-1"
+    encrypt = true
+    acl = "private"
   }
 }
